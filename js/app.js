b8fbf1fc40008b42e8c152ee180a211dcb974f4b
sam weeks <weeksy_17@icloud.com>
2020-03-21 01:26:27 +0000

    add new

--- js/app.js
+++ js/app.js
@@ -6,19 +6,77 @@ $('.lightbox-wrap').magnificPopup({
 
 });
 
-$('.lightbox-wrap').each(function() { // the containers for all your galleries
-    $(this).magnificPopup({
-        delegate: 'a', // the selector for gallery item
-        type: 'image',
-        gallery: {
-          enabled:true,
-          preload: [0,2],
-          closeBtnInside: false
-      }
+// $('.lightbox-wrap').each(function() { // the containers for all your galleries
+//     $(this).magnificPopup({
+//         delegate: 'a', // the selector for gallery item
+//         type: 'image',
 
-  });
+    
+//         gallery: {
+//           enabled:true,
+//           preload: [0,2],
+//           closeBtnInside: false
+//       }
+
+//   });
+// });
+
+$('.lightbox-wrap').click( function () {
+  $(this). magnificPopup({
+    items: [
+      {
+        src: 'photo_gallery_v4/photos/01.jpg',
+        title: 'Hay Bales in a golden field'
+      },
+       {
+        src: 'photo_gallery_v4/photos/02.jpg',
+        title: 'Mountains and Sea'
+      },
+       {
+        src: 'photo_gallery_v4/photos/03.jpg',
+        title: 'Green Beauty'
+      },
+       {
+        src: 'photo_gallery_v4/photos/04.jpg',
+        title: 'Snowscape '
+      },
+       {
+        src: 'photo_gallery_v4/photos/05.jpg',
+        title: 'Utah desert'
+      },
+       {
+        src: 'photo_gallery_v4/photos/06.jpg',
+        title: 'Autumn colours'
+      },
+       {
+        src: 'photo_gallery_v4/photos/07.jpg',
+        title: 'Pure Green'
+      },
+       {
+        src: 'photo_gallery_v4/photos/08.jpg',
+        title: 'Desert'
+      },
+       {
+        src: 'photo_gallery_v4/photos/09.jpg',
+        title: 'country road'
+      },
+       {
+        src: 'photo_gallery_v4/photos/10.jpg',
+        title: 'rivers and sunsets'
+      },
+       {
+        src: 'photo_gallery_v4/photos/11.jpg',
+        title: 'we need to fix this hole'
+      },
+       {
+        src: 'photo_gallery_v4/photos/12.jpg',
+        title: 'Another Mountain'
+      },
+
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
 });
-
-$('div a img caption').hide();
-
-
+});
